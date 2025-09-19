78019a5c3620fdd9f6f1fb45dbcd5b19c499d487
Zhang Weijun <76138023+Hello-Blanki@users.noreply.github.com>
2025-09-19 14:23:23 +0800

    OUC_LidarGroup

--- Hurdles/RunExample.m
+++ Hurdles/RunExample.m
@@ -11,7 +11,7 @@
 clearvars;close all;clc;
 
 % select LDP function
-MyLDPfunction       = @LDP_v3; % [LDP_v3/???]
+MyLDPfunction       = @LDP_OUC; % [LDP_v3/???]
 
 % select LidarType
 LidarType           = '4BeamPulsed'; % [4BeamPulsed/CircularCW]
